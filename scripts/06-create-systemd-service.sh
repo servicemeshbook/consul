7b576f7f12a3ca92d00114ff540f9e4c7016f79b
root <root@osc01.servicemesh.local>
2019-05-13 08:01:25 -0400

    first commit

---
+++ scripts/06-create-systemd-service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Use absolute path for the go bonary
+
+cat << EOF | tee /etc/systemd/system/external-counting.service
+  [Unit]
+  Description = "External Counting Service"
+  
+  [Service]
+  KillSignal=INT
+  Environment="PORT=10001"
+  ExecStart=/root/counting-service
+  Restart=always
+
+  [Install]
+  WantedBy=multi-user.target
+EOF
